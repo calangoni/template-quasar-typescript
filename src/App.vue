ab21516d83664dea98ba9e57369a4b377e755b2a
Carlos Langoni <calangoni@gmail.com>
2018-03-22 16:51:33 -0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <router-view />
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+
+@Component({
+  name: 'App'
+})
+export default class App extends Vue {
+}
+</script>
+
+<style>
+</style>
